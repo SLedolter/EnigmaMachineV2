4a3c07bfb0b37a7813902b85bc510c0670f0194e
Stefan Ledolter <s.ledolter@gmail.com>
2022-06-02 18:52:34 +0200

    SL: added test for encoding after reflector

--- EnigmaMachineV2Tests/CylinderTests.cs
+++ EnigmaMachineV2Tests/CylinderTests.cs
@@ -6,11 +6,21 @@ namespace EnigmaMachineV2Tests {
   [TestClass]
   public class CylinderTests {
     [TestMethod]
-    public void Test_Roto1_Encoding() {
+    public void Test_Rotor1_Encoding_Before_Reflector() {
       Cylinder rotor1 = new Cylinder("I", "A", EnigmaConfig.TURNOVER_1_CYLINDER_1, EnigmaConfig.CYLINDER_1);
 
       char expected = 'E';
       char result = rotor1.EncodeLetter('A', true);
+
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void Test_Rotor1_Encoding_After_Reflector() {
+      Cylinder rotor1 = new Cylinder("I", "A", EnigmaConfig.TURNOVER_1_CYLINDER_1, EnigmaConfig.CYLINDER_1);
+
+      char expected = 'U';
+      char result = rotor1.EncodeLetter('A', false);
 
       Assert.AreEqual(expected, result);
     }
