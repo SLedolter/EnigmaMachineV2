106555405f58585d1eccbedd0244acf6da88fbde
Stefan Ledolter <s.ledolter@gmail.com>
2022-06-02 21:04:05 +0200

    SL: moved ui code into own class

---
+++ EnigmaMachineV2/ConsoleUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaMachineV2 {
+  public class ConsoleUI {
+    EnigmaMachine enigmaMachine;
+
+    public ConsoleUI(EnigmaMachine enigmaMachine) {
+      this.enigmaMachine = enigmaMachine;
+    }
+
+    public void ShowUI() {
+      string command;
+      do {
+        Console.CursorTop = Console.WindowHeight - 2;
+        Console.CursorLeft = 1;
+        Console.Write(">> ");
+        command = Console.ReadLine();
+
+        if (command == "1") {
+          char input;
+          string plainMessage = "";
+          string encodedMessage = "";
+          Console.CursorTop = Console.WindowHeight - 2;
+          Console.CursorLeft = 1;
+          Console.Write("ENC>> ");
+          while ((input = char.ToUpper(Console.ReadKey().KeyChar)) != '-') {
+            char result = enigmaMachine.EncodeLetter(input);
+
+            if (result == ' ') {
+              continue;
+            }
+
+            plainMessage += input;
+            encodedMessage += result;
+
+            Console.CursorTop = 1;
+            Console.CursorLeft = 1;
+            Console.WriteLine(plainMessage);
+            Console.CursorLeft = 1;
+            Console.WriteLine(encodedMessage);
+
+            Console.CursorTop = Console.WindowHeight - 2;
+            Console.CursorLeft = 1;
+            Console.Write("ENC>> ");
+          }
+        }
+
+      } while (command != "exit");
+    }
+  }
+}
--- EnigmaMachineV2/Program.cs
+++ EnigmaMachineV2/Program.cs
@@ -8,44 +8,9 @@ namespace EnigmaMachineV2 {
   class Program {
     static void Main(string[] args) {
       EnigmaMachine enigmaMachine = EnigmaMachineFactory.CreateSimpleRotor1ReflectorEnigmaMachine("A");
+      ConsoleUI consoleUI = new ConsoleUI(enigmaMachine);
 
-      string command;
-      do {
-        Console.CursorTop = Console.WindowHeight - 2;
-        Console.CursorLeft = 1;
-        Console.Write(">> ");
-        command = Console.ReadLine();
-        
-        if(command == "1") {
-          char input;
-          string plainMessage = "";
-          string encodedMessage = "";
-          Console.CursorTop = Console.WindowHeight - 2;
-          Console.CursorLeft = 1;
-          Console.Write("ENC>> ");
-          while ((input = char.ToUpper(Console.ReadKey().KeyChar)) != '-'){
-            char result = enigmaMachine.EncodeLetter(input);
-
-            if (result == ' ') {
-              continue;
-            }
-
-            plainMessage += input;
-            encodedMessage += result;
-
-            Console.CursorTop = 1;
-            Console.CursorLeft = 1;
-            Console.WriteLine(plainMessage);
-            Console.CursorLeft = 1;
-            Console.WriteLine(encodedMessage);
-
-            Console.CursorTop = Console.WindowHeight - 2;
-            Console.CursorLeft = 1;
-            Console.Write("ENC>> ");
-          }
-        }
-
-      } while (command != "exit");
+      consoleUI.ShowUI();
     }
   }
 }
