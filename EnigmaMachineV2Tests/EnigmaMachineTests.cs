47cb17a8b9749bc64f7e37fb0000b8081ecbdf66
Stefan Ledolter <s.ledolter@gmail.com>
2022-06-02 20:59:18 +0200

    SL: fixed problem with enigmaMachine tests;

--- EnigmaMachineV2Tests/EnigmaMachineTests.cs
+++ EnigmaMachineV2Tests/EnigmaMachineTests.cs
@@ -5,6 +5,7 @@ using EnigmaMachineV2;
 namespace EnigmaMachineV2Tests {
   [TestClass]
   public class EnigmaMachineTests {
+    [TestMethod]
     public void Test_Simple_Enigma_Rotor1_Reflector_No_Stepping_With_A() {
       EnigmaMachine enigmaMachine = new EnigmaMachine("Rotor 1 - Reflector");
 
@@ -14,3 +15,4 @@ namespace EnigmaMachineV2Tests {
     }
   }
 }
+
