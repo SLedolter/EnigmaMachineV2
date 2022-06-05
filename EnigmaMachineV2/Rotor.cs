3a4a14f7070b803b94004d8911cbde2a7c48e9e1
Stefan Ledolter <s.ledolter@gmail.com>
2022-06-05 21:01:24 +0200

    SL: added debug for mapping to find bugs

--- EnigmaMachineV2/Rotor.cs
+++ EnigmaMachineV2/Rotor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,7 @@ namespace EnigmaMachineV2 {
         charIndex %= 26;
         result += EnigmaConfig.ALPHABET[charIndex];
       }
+      Debug.WriteLine($"{name}:{mapping} {result}");
       mapping = result;
     }
 
